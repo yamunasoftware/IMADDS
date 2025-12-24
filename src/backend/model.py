b2926c681f9f7e19071fce4f74c0b40b97ee0d9d
Sushen Kolakaleti <yamunasoftware18@gmail.com>
2025-12-23 21:49:00 -0500

    Updated Dependencies

--- src/backend/model.py
+++ src/backend/model.py
@@ -1,1 +1,8 @@
-from pyspark.ml.classification import RandomForestClassifier
+import pickle
+
+def load_model():
+  with open('/main/src/ddm.pkl', 'rb') as f:
+    return pickle.load(f)
+
+def predict_model(model, data):
+  return model.predict(data)
